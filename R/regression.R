c8de149bb3dcccf9828523d065d9fcbe51cbe321
DannyArends <Danny.Arends@Gmail.com>
2010-09-23 14:17:12 +0200

    Minor changes to the R-code

--- R/regression.R
+++ R/regression.R
@@ -10,22 +10,22 @@ testregression <-function(){
 
 multipleregression <- function(designmatrix,weight,y,verbose=FALSE){
   if(length(weight) != length(y)){
-    stop("Not all samples have a weight, length(weight) != length(y)")
+    stop("Not all samples have a weight, length(weight) != length(y) (Values:",length(weight)," != ",length(y),")")
   }
   if(nrow(designmatrix) != length(y)){
-    stop("Not all samples have a output, nrow(designmatrix) != length(y)")
+    stop("Not all samples have a output, nrow(designmatrix) != length(y) (Values:",nrow(designmatrix)," != ",length(y),")")
   }
   if(!sum(designmatrix[,1])==length(y)){
     warning("Adding estimate of constant in model")
     designmatrix <- cbind(rep(1,length(y)),designmatrix)
   }
   result <- .C("lodscorebyem_R",nvariables=as.integer(ncol(as.matrix(designmatrix))),
-                                  nsamples=as.integer(nrow(as.matrix(designmatrix))),
-                                  x=as.matrix(designmatrix),
-                                  w=weight,
-                                  y=y,
-                                  verbose=as.integer(verbose),
-                                  likelihood=0)
+                                nsamples=as.integer(nrow(as.matrix(designmatrix))),
+                                x=as.matrix(designmatrix),
+                                w=weight,
+                                y=y,
+                                verbose=as.integer(verbose),
+                                likelihood=0)
   result
 }
 
