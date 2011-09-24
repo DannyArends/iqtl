70f3ac212c98408d7921f5fc77dc234f61cdb38e
DannyArends <Danny.Arends@gmail.com>
2011-09-24 12:04:37 +0200

    Able to send strings from R->D
    Now still need to get R_alloc to work, so we can allocate 'safe' memory
    for storing output results in. getLine is still crashing because it
    cannot allocate the required memory, or corrupts the other parts.

--- D/raux.d
+++ D/raux.d
@@ -8,7 +8,6 @@ string[] getStringArrayFromR(int* r_length, int* r_sizes, char** r_values){
   int[] sizes = r_sizes[0..(*r_length)];
   string values[];
   for(int s=0;s<(*r_length);s++){
-    writefln("s: %s",s);
     string tmp;
     for(int c=0;c<sizes[s];c++){
 	  tmp ~= r_values[s][c];
@@ -16,4 +15,13 @@ string[] getStringArrayFromR(int* r_length, int* r_sizes, char** r_values){
     values ~= tmp;
   }
   return values;
+}
+
+string intToString(int[] bits){
+  char[] r;
+  foreach(int b;bits){
+    r ~= cast(char)(b);
+  }
+  //r ~= '\0';
+  return to!string(r);
 }
--- R/Dfunctions.R
+++ R/Dfunctions.R
@@ -1,34 +1,49 @@
-library(R.lang)
-dyn.load("Dfunctions.dll")
+#
+#Functions inside D shared library
+#First run: ./D/compile.bat for .dll
+#First run: ./D/compile.sh for .so
+#
 
-#setwd("E:/GBIC/Arabidopsis_tiling")
-#toSend <- 1:100
-
-toSend <- "Hallo Wereld!"
-
-.C("CharTest",as.integer(length(toSend)), as.integer(nchar(toSend)),as.character(toSend))
-
-getLine <- function(filename = "exp_ann.txt", linenumber = c(10,100,100000), header=TRUE, sep='\t', quoted=TRUE){
+StringArrayToD <- function(toSend = c("Hallo Wereld!","R->D->R")){
   file <- "Dfunctions.dll"
-  if(!is.loaded("loadLineFromFile")) {
+  dfunct <- "CharTest"
+  if(!is.loaded(dfunct)){
+    dyn.load(file)
+    cat(" -Loading:", file, "\n")
+  }
+  if(is.loaded(dfunct)){
+    .C("CharTest",as.integer(length(toSend)), as.integer(nchar(toSend)),as.character(toSend))
+  }else{
+    stop("Function",dfunct,"not in",file)
+  }
+}
+
+getLine <- function(filename = "exp_ann.txt", linenumbers = c(10,100,100000), header=TRUE, sep='\t', quoted=TRUE){
+  file <- "Dfunctions.dll"
+  dfunct <- "loadLineFromFile"
+  if(!is.loaded(dfunct)) {
     dyn.load(file)
     cat(" -Loaded", file, "\n")
   }
-  output <- 1:10
-  OUT <- .C("loadLineFromFile",
-      as.integer(getBytes(filename)),
-      as.integer(linenumber),
+  if(is.loaded(dfunct)){
+    output <- 0
+    OUT <- .C("loadLineFromFile",
+      as.character(filename),
+      as.integer(linenumbers),
       as.integer(header),
-      getBytes(sep),
+      as.character(sep),
       as.integer(quoted),
+      as.integer(length(filename)),
       as.integer(nchar(filename)),
-      as.integer(length(linenumber)),
+      as.integer(length(linenumbers)),
       as.integer(output))
-  OUT
+    OUT
+  }else{
+    stop("Function",dfunct,"not in",file)
+  }
 }
 
+#StringArrayToD()
 #testdata <- getLine()
 #testdata
-#paste(intToChar(testdata),collapse="")
-
 #q("no")
