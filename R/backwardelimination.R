008024afddf2d2a7e336b3a3d6d6487b4521cded
DannyArends <Danny.Arends@gmail.com>
2011-09-24 16:22:28 +0200

    Fixed model selection by multiple regression
    There was a bug where missing reponse variables would not be dropped
    from the analysis. This was seen in the multitrait set, fixed the bug
    and added a test to catch this in the future.

--- R/backwardelimination.R
+++ R/backwardelimination.R
@@ -18,12 +18,26 @@ inverseF <- function(df1,df2,alpha=0.05){
 }
 
 backwardeliminate <- function(cross,pheno.col=1,type=0,cofactors,alpha=0.01,verbose=FALSE){
+  if(length(pheno.col) > 1){
+    return(lapply(pheno.col,function(x){
+      if(x <= nphe(cross) && x > 0){
+        backwardeliminate(cross=cross,pheno.col=x,type=type,cofactors=cofactors,alpha=alpha,verbose=verbose)
+      }
+    }))
+  }
   s <- proc.time()
+  if(missing(cross)) stop("No cross object")
+  if(missing(cofactors)) stop("No cofactors, please supply cofactors")
   contrastlist  <- crosstocontrastlist(cross,type)
   contrastlist  <- lapply(FUN=scaledowncontrast,contrastlist)
+  out_of_range  <- which(cofactors > length(contrastlist) | cofactors < 1)
+  if(length(out_of_range) > 0){
+    warning("Some cofactors are out of range: ",cofactors[out_of_range])
+    cofactors <- cofactors[-out_of_range];
+  }
   model         <- cofactors
   finished      <- FALSE
-  pheno         <- cross$pheno[,pheno.col] 
+  pheno         <- cross$pheno[,pheno.col]
   thismarkerclist <- contrastlisttodesignmatrix(contrastlist,model)
   logLfull      <- modellikelihood(thismarkerclist,pheno)$likelihood
   dropneeded    <- 2*inverseF(2,nrow(thismarkerclist)-length(model),alpha);
@@ -41,8 +55,8 @@ backwardeliminate <- function(cross,pheno.col=1,type=0,cofactors,alpha=0.01,verb
       thismarkerclist <- contrastlisttodesignmatrix(contrastlist,tempmodel)
       loglikelyhood = c(loglikelyhood,modellikelihood(thismarkerclist,pheno)$likelihood)
     }
-    leastinterestingmodel = which.max(loglikelyhood);
-    likelihoodleastinterestingmodel = max(loglikelyhood);
+    leastinterestingmodel = which.max(loglikelyhood)
+    likelihoodleastinterestingmodel = max(loglikelyhood)
     e <- proc.time()
     reduction <- as.numeric(e[3]-s[3])
     if(dropneeded > abs(logLfull - likelihoodleastinterestingmodel)){
@@ -50,18 +64,21 @@ backwardeliminate <- function(cross,pheno.col=1,type=0,cofactors,alpha=0.01,verb
       if(verbose) cat("Dropped variable",model[leastinterestingmodel],":",logLfull,"after",reduction,"sec\n");
       model <- model[-leastinterestingmodel]
     }else{
-      if(verbose){
-        cat("We have a model\n");
-        for(x in 1:length(model)){
-          cat("Variable",model[x],"in Model\n");
-        }
-      }
       finished=TRUE;
     }
   }
+  if(!finished && dropneeded > abs(logLfull - min(loglikelyhood))){
+    model <- model[-which.min(loglikelyhood)]
+  }
   if(verbose){
-    cat("startup:",startup,"sec\n")
-    cat("modeling:",reduction,"sec\n")
+    cat("Model: ");
+    for(x in 1:length(model)){
+      cat("Variable",model[x],"in Model\n");
+    }
+  }
+  if(verbose){
+    cat("Startup:",startup,"sec\n")
+    cat("Modeling:",reduction,"sec\n")
   }
   model
 }
