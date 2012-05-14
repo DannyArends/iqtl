a6634f999f249b893d7485934463a145823468ac
DannyArends <Danny.Arends@gmail.com>
2012-05-14 13:25:13 +0200

    [import] SRC of qtlHD

---
+++ SRC/iqtlhd.h
@@ -0,0 +1,14 @@
+/**********************************************************************
+ * src/iqtlhd.h
+ *
+ * copyright (c) 2012 Danny Arends
+ * last modified Jan, 2012
+ * first written Jan, 2012
+ **********************************************************************/
+
+#ifndef __IQTLHD_H
+  #define __IQTLHD_H
+
+#include <R.h>
+
+#endif // IQTLHD_H
