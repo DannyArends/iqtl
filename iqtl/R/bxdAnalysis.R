6f430d9e3d31bc191623131083453f06f9bdd6e9
DannyArends <Danny.Arends@gmail.com>
2013-10-01 13:13:42 +0200

    Updates to make the package installable again

--- iqtl/R/bxdAnalysis.R
+++ iqtl/R/bxdAnalysis.R
@@ -1,6 +1,7 @@
-setwd("~/Desktop/GeneNetwork")
-bxdGeno       <- read.csv("BXD.geno", skip=6, sep="\t")
-bxdPhenoDescr <- read.csv("BXD.phenome",sep='\t',row.names=1)
+
+#setwd("~/Desktop/GeneNetwork")
+#bxdGeno       <- read.csv("BXD.geno", skip=6, sep="\t")
+#bxdPhenoDescr <- read.csv("BXD.phenome",sep='\t',row.names=1)
 
 getPhenotype <- function(probeset = "13399", DB = "BXDPublish"){
   protocol <- "http"
@@ -9,39 +10,47 @@ getPhenotype <- function(probeset = "13399", DB = "BXDPublish"){
   return(request, read.csv(, sep = "\t", row.names=1, header=TRUE))
 }
 
-strains <- c("C57BL/6J", "DBA/2J", "B6D2F1", paste0("BXD", 1:103))
+possibleBXDstrains <- function(){ return(c("C57BL/6J", "DBA/2J", "B6D2F1", paste0("BXD", 1:103)))}
 
-if(!file.exists("BXD.pheno.17sept13")){
-  phenotypes <- matrix(NA, length(strains), nrow(bxdPhenoDescr))
-  rownames(phenotypes) <- strains
-  colnames(phenotypes) <- rownames(bxdPhenoDescr)
-  for(x in 1:nrow(bxdPhenoDescr)){
-    recordid <- rownames(bxdPhenoDescr)[x]
-    phenotype <- getPhenotype(recordid)                                                   # Whole data from geneNetwork
-    avail <- which(rownames(phenotype) %in% rownames(phenotypes))                         # We sometimes download data for other BXDs
-    phenotypes[rownames(phenotype)[avail], x] <- as.numeric(unlist(phenotype)[avail])     # Storage the available downloaded data
-    if(x %% 100 == 0) cat("Downloaded", x,"phenotypes\n")
+createPhenotypes <- function(){
+  if(!file.exists("BXD.pheno.17sept13")){
+    phenotypes <- matrix(NA, length(strains), nrow(bxdPhenoDescr))
+    rownames(phenotypes) <- strains
+    colnames(phenotypes) <- rownames(bxdPhenoDescr)
+    for(x in 1:nrow(bxdPhenoDescr)){
+      recordid <- rownames(bxdPhenoDescr)[x]
+      phenotype <- getPhenotype(recordid)                                                   # Whole data from geneNetwork
+      avail <- which(rownames(phenotype) %in% rownames(phenotypes))                         # We sometimes download data for other BXDs
+      phenotypes[rownames(phenotype)[avail], x] <- as.numeric(unlist(phenotype)[avail])     # Storage the available downloaded data
+      if(x %% 100 == 0) cat("Downloaded", x,"phenotypes\n")
+    }
+    write.table(file="BXD.pheno.17sept13", phenotypes,sep='\t',quote=FALSE)
+  }else{
+    phenotypes <- read.csv("BXD.pheno.17sept13",sep='\t',check.names=FALSE)
   }
-  write.table(file="BXD.pheno.17sept13", phenotypes,sep='\t',quote=FALSE)
-}else{
-  phenotypes <- read.csv("BXD.pheno.17sept13",sep='\t',check.names=FALSE)
+  return(phenotypes)
 }
 
 # Modify the genotypes
-nGeno <- t(bxdGeno[,-c(1:4)])
-nGeno[nGeno=="B"] <- -1
-nGeno[nGeno=="H"] <- 0
-nGeno[nGeno=="D"] <- 1
-nGeno <- apply(nGeno, 2, as.numeric)
-rownames(nGeno) <- colnames(bxdGeno)[-c(1:4)]
-colnames(nGeno) <- bxdGeno[,"Locus"]
+modifyGenotypes <- function(){
+	nGeno <- t(bxdGeno[,-c(1:4)])
+	nGeno[nGeno=="B"] <- -1
+	nGeno[nGeno=="H"] <- 0
+	nGeno[nGeno=="D"] <- 1
+	nGeno <- apply(nGeno, 2, as.numeric)
+	rownames(nGeno) <- colnames(bxdGeno)[-c(1:4)]
+	colnames(nGeno) <- bxdGeno[,"Locus"]
+}
 
-noPhenotype <- which(apply(phenotypes,1,function(x){sum(is.na(x))})==ncol(phenotypes))
-phenotypes <- phenotypes[-noPhenotype,]
+modifyPhenotypes <- function(){
+  noPhenotype <- which(apply(phenotypes,1,function(x){sum(is.na(x))})==ncol(phenotypes))
+  phenotypes <- phenotypes[-noPhenotype,]
+}
 
 # Map QTLs
-cnt <- 1
-lods <- apply(phenotypes[,1:100], 2, function(phenotype){
+mapQTLs <- function(){
+  cnt <- 1
+  lods <- apply(phenotypes[,1:100], 2, function(phenotype){
 	lod <- apply(nGeno, 2, function(marker){
 		model <- lm(as.numeric(phenotype[names(marker)]) ~ marker)
 		-log10(anova(model)[[5]][1])
@@ -49,7 +58,8 @@ lods <- apply(phenotypes[,1:100], 2, function(phenotype){
 	cat(paste0("Done phenotype ", cnt, "\n"));
 	cnt <<- cnt+1
 	return(lod)
-})
+  })
+}
 
 toCol <- function(x){
   if(x < 3)  return(rgb(0.8,  0.8, 0.8))
@@ -87,11 +97,14 @@ plotQTL <- function(bxdGeno, lods, trait = 1, use = c("cM","Mb")){
     })
     unlist(lapply(lodlocs, function(x){round(max(x),d=1)}))
 }
-plotQTL(bxdGeno, lods, 8)
 
-op <- par(mai=c(3, 4, 0.5, 0.5))
-image(1:ncol(lods),1:ncol(lods), cor(lods), xaxt='n', yaxt='n', ylab="", xlab="", breaks = c(-1, -0.5, -0.2, 0.2, 0.5, 1), col=c("red", "orange", "white", "lightblue", "blue"))
-axis(1, at=1:ncol(lods), colnames(lods), las=2, cex.axis=0.7)
-axis(2, at=1:ncol(lods), colnames(lods), las=1, cex.axis=0.8)
-abline(h=seq(0.5,ncol(lods), 1),col='gray',lty=2)
-abline(v=seq(0.5,ncol(lods), 1),col='gray',lty=2)
+createPLots <- function(){
+	plotQTL(bxdGeno, lods, 8)
+
+	op <- par(mai=c(3, 4, 0.5, 0.5))
+	image(1:ncol(lods),1:ncol(lods), cor(lods), xaxt='n', yaxt='n', ylab="", xlab="", breaks = c(-1, -0.5, -0.2, 0.2, 0.5, 1), col=c("red", "orange", "white", "lightblue", "blue"))
+	axis(1, at=1:ncol(lods), colnames(lods), las=2, cex.axis=0.7)
+	axis(2, at=1:ncol(lods), colnames(lods), las=1, cex.axis=0.8)
+	abline(h=seq(0.5,ncol(lods), 1),col='gray',lty=2)
+	abline(v=seq(0.5,ncol(lods), 1),col='gray',lty=2)
+}
